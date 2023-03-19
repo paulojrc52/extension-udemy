668fa82767951e4e7a2d7dd6f02c7454c7fda028
Paulo Júnior <paulojrcosta300@gmail.com>
2023-03-19 17:46:34 -0300

    Udemy full screen extension framework

---
+++ script.js
@@ -0,0 +1,45 @@
+
+function startApp(containerVideo, contentVideoPlayer) {
+  const bgFull = document.createElement('div')
+  bgFull.classList.add('bg-full')
+  let isFullScreen = false
+  
+  const btn = document.createElement('div')
+  btn.classList.add('btn-fullscreen')
+  btn.innerText = 'FULL'
+  containerVideo.appendChild(btn)
+  
+  btn.onclick = function() {
+    if(!isFullScreen) {
+      isFullScreen = true
+      bgFull.classList.add('bg-full')
+      document.body.appendChild(bgFull)
+      bgFull.appendChild(containerVideo)
+      const videoStream = document.querySelector('.vjs-tech')
+      videoStream.style.width = '100%'
+      videoStream.style.height = '100%'
+    } 
+  }
+
+  document.addEventListener('keyup', event => {
+    if(event.key === 'Escape' && isFullScreen){  
+      isFullScreen = false
+      contentVideoPlayer.appendChild(containerVideo)
+      bgFull.remove()
+    }
+  })
+
+}
+  
+const interval = setInterval(() => { 
+  const containerVideo = document.querySelector('.video-player--video-wrapper--1L212')
+  
+  console.log(containerVideo)
+  if(containerVideo) {
+    const contentVideoPlayer = containerVideo.parentElement
+    console.log(contentVideoPlayer)
+    clearInterval(interval)
+    startApp(containerVideo, contentVideoPlayer)
+  }
+}, 50)
+ 
